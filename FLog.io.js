1f4f113bd19ec0b583976a9a82cd2cb2028b759f
FooeyBar <robi.medeiros.3339@gmail.com>
2020-08-02 19:25:27 -0400

    v3.0.1 -> added remove() function

--- FLog.io.js
+++ FLog.io.js
@@ -10,67 +10,103 @@ const flog=(()=>{
             ,parents:typeof 'object'
             ,children:typeof 'object'          
         }){
-        const self=this;
-        let newconfig={};
-        
-        if(_config.name===undefined||typeof _config.name!=='string')Object.defineProperty(newconfig,'name',{value:flogname,enumerable:true,writable:false});
-        else Object.defineProperty(newconfig,'name',{value:_config.name,enumerable:true,writable:false});
-        
-        if(_config.writeconsole===undefined||typeof _config.writeconsole!=='boolean')Object.defineProperty(newconfig,'writeconsole',{value:true,enumerable:true,writable:true});
-        else Object.defineProperty(newconfig,'writeconsole',{value:_config.writeconsole,enumerable:true,writable:false});
-        
-        if(_config.readconsole===undefined||typeof _config.readconsole!=='boolean')Object.defineProperty(newconfig,'readconsole',{value:false,enumerable:true,writable:true});
-        else Object.defineProperty(newconfig,'readconsole',{value:_config.readconsole,enumerable:true,writable:false});
-        
-        if(_config.sockets===undefined||typeof _config.sockets!=='object')Object.defineProperty(newconfig,'sockets',{value:[],enumerable:true,writable:true});
-        else{
-            let arr=[];
-            for(let i in _config.sockets){
-                if(typeof _config.sockets[i].sockets.emit==='function'){arr.push(_config.sockets[i]);}
-                else{console.log(_config.sockets[i],'^not a valid socket > emit is not a function');}
-            }
-            Object.defineProperty(newconfig,'sockets',{value:arr,enumerable:true,writable:true});
-        }
-
-        if(_config.emitname===undefined||typeof _config.emitname!=='string')Object.defineProperty(newconfig,'emitname',{value:flogname,enumerable:true,writable:true});
-        else Object.defineProperty(newconfig,'emitname',{value:_config.emitname,enumerable:true,writable:false});
-        
-        if(_config.parents===undefined||typeof _config.parents!=='object')Object.defineProperty(newconfig,'parents',{value:[],enumerable:true,writable:true});
-        else{
-            let arr=[];
-            for(let i in _config.parents){
-                if(_config.parents[i] instanceof io){arr.push(_config.parents[i]);}
-                else{console.log(_config.parents[i],'^not an instance of '+flogname);}
-            }
-            Object.defineProperty(newconfig,'parents',{value:arr,enumerable:true,writable:true});
-        }
-
-        if(_config.children===undefined||typeof _config.children!=='object')Object.defineProperty(newconfig,'children',{value:[],enumerable:true,writable:true});
-        else{
-            let arr=[];
-            for(let i in _config.children){
-                if(_config.children[i] instanceof io){arr.push(_config.children[i]);}
-                else{console.log(_config.children[i],'^not an instance of '+flogname);}
-            }
-            Object.defineProperty(newconfig,'children',{value:arr,enumerable:true,writable:true});
-        }
-
-        this.config=newconfig;
-        delete newconfig;
         //==================================================================================================================
-        let send=(_msg)=>{
+        this.print=function(){
+            try{
+                let arr=Array.from(arguments);
+                _print(arr,false);
+                for(let i in self.config.parents){
+                    for(let j=0;j<arr.length;j++){self.config.parents[i](arr[j]);}
+                }
+            }
+            catch(err){console.log(err,self.config.name+'.print() error > check arguments:',arguments);}
+        };
+        this.addsocket=(_socket)=>{
+            if(typeof _socket.emit==='undefined')this.print(_socket,'^emit: not found');
+            else if(typeof _socket.emit==='function'){
+                for(let i=0;i<this.config.sockets.length;i++){
+                    if(this.config.sockets[i]===_socket){
+                        this.print(this.config.name+'.addsocket() ->',_socket,'^duplicate socket');
+                        return;
+                    }}
+                try{if(send('test message',true,_socket))this.config.sockets.push(_socket);}
+                catch(err){this.print(err);}
+            }
+            else this.print(_socket,'^emit: not a function');
+        };
+        this.addchild=(_child)=>{
+            if(_child instanceof flog.io){
+                if(!family(_child)){
+                    _child.config.writeconsole=false;
+                    _child.config.parents.push(this.print);
+                    this.config.children.push(_child);
+                }
+                else{this.print(_child,'^already a child');}
+            }
+            else this.print(this.config.name+'.addchild() ->',_child,'^not an instance of '+flogname);
+        };
+        this.addparent=(_parent)=>{
+            if(_parent instanceof flog.io){
+                if(!family(_parent)){
+                    this.config.writeconsole=false;
+                    this.config.parents.push(_parent.print);
+                    _parent.config.children.push(this);
+                }
+                else{this.print(_parent,'^already a parent');}
+            }
+            else this.print(this.config.name+'.addparent() ->',_parent,'^not an instance of '+flogname);
+        };
+        this.remove=(_obj)=>{
+            if(_removesocket(_obj)){return;}
+            if(_removechild(_obj)){return;}
+            if(_removeparent(_obj)){return;}
+            {this.print(this.config.name+'.remove() ->',_obj,'^not found');}
+        };
+        //===================================================
+        let _removesocket=(_socket)=>{
+            for(let i=0;i<this.config.sockets.length;i++){
+                if(this.config.sockets[i]===_socket){
+                    this.config.sockets.splice(i,1);
+                    return true;
+                }
+            }
+            return false;
+        };
+        let _removechild=(_child)=>{
+            for(let i=0;i<this.config.children.length;i++){
+                if(this.config.children[i]===_child){
+                    this.config.children.splice(i,1);
+                    if(_child.config.parents.length===0)_child.config.writeconsole=true;
+                    return true;
+                }
+            }
+            return false;
+        };
+        let _removeparent=(_parent)=>{
+            for(let i=0;i<this.config.parents.length;i++){
+                if(this.config.parents[i]===_parent){
+                    this.config.parents.splice(i,1);
+                    if(this.config.parents.length===0){this.config.writeconsole=true;}
+                    return true;
+                }
+            }
+            return false;
+        };
+        let send=(_msg,test,sock)=>{
+            let emit=(socket,msg)=>{
+                try{socket.emit(this.config.emitname,msg);return true;}
+                catch(err){this.print(err);return false;}
+            };
+            if(test){return emit(sock,_msg);}
             for(let s=0;s<this.config.sockets.length;s++){
                 if(typeof(_msg)==='object'){
                     let temp='{';
-                    for(let i in _msg){
-                        if(typeof(_msg[i])==='function'){temp+=' '+i+': function,';}
-                        else {temp+=' '+i+': '+_msg[i]+',';}
-                    }
+                    for(let i in _msg){temp+=(typeof(_msg[i])==='function')?(' '+i+': function,'):(' '+i+': '+_msg[i]+',');}
                     temp=temp.slice(0,temp.length-1);
                     temp+=' }';
-                    this.config.sockets[s].emit(this.config.emitname,temp);
+                    emit(this.config.sockets[s],temp);
                 }
-                else{this.config.sockets[s].emit(this.config.emitname,_msg);}
+                else{emit(this.config.sockets[s],_msg);}
             }
         };
         let _print=(_input,_fromconsole)=>{
@@ -90,54 +126,59 @@ const flog=(()=>{
                 }
             }
         };
-        this.print=function(){
-            try{
-                let arr=Array.from(arguments);
-                _print(arr,false);
-                for(let i in self.config.parents){
-                    for(let j=0;j<arr.length;j++){self.config.parents[i](arr[j]);}
-                }
-            }
-            catch(err){console.log(err,self.config.name+'.print() error > check arguments:',arguments);}
-        };
-        this.addsocket=(_socket)=>{
-            try{
-                if(typeof _socket.emit==='function'){
-                    this.config.sockets.push(_socket);
-                    this.print('socket set');
-                }
-                else this.print(_socket,'^not a valid socket > emit is not a function');
-            }
-            catch(err){console.log(err);}
-        };
         let family=(obj)=>{
-            for(let i in this.config.children)if(i===obj)return true;
-            for(let i in this.config.parents)if(i===obj)return true;
-        };
-        this.addchild=(_child)=>{
-            if(_child instanceof flog.io){
-                if(!family(_child)){
-                    _child.config.writeconsole=false;
-                    _child.config.parents.push(this.print);
-                }
-            }
-            else this.print(this.config.name+'.add() ->',_child,'^not an instance of '+flogname);
-        };
-        this.addparent=(_parent)=>{
-            if(_parent instanceof flog.io){
-                if(!family(_parent)){
-                    this.config.writeconsole=false;
-                    this.config.parents.push(_parent.print);
-                }
-            }
-            else this.print(this.config.name+'.add() ->',_parent,'^not an instance of '+flogname);
+            for(let i=0;i<this.config.children.length;i++)if(this.config.children[i]===obj){return true;}
+            for(let i=0;i<this.config.parents.length;i++)if(this.config.parents[i]===obj){return true;}
+            return false;
         };
         //==================================================================================================================
+        let newconfig={};
+        if(_config.name===undefined||typeof _config.name!=='string')Object.defineProperty(newconfig,'name',{value:flogname,enumerable:true,writable:false});
+        else Object.defineProperty(newconfig,'name',{value:_config.name,enumerable:true,writable:false});
+
+        if(_config.writeconsole===undefined||typeof _config.writeconsole!=='boolean')Object.defineProperty(newconfig,'writeconsole',{value:true,enumerable:true,writable:true});
+        else Object.defineProperty(newconfig,'writeconsole',{value:_config.writeconsole,enumerable:true,writable:false});
+
+        if(_config.readconsole===undefined||typeof _config.readconsole!=='boolean')Object.defineProperty(newconfig,'readconsole',{value:false,enumerable:true,writable:false});
+        else Object.defineProperty(newconfig,'readconsole',{value:_config.readconsole,enumerable:true,writable:false});
+
+        if(_config.sockets===undefined||typeof _config.sockets!=='object')Object.defineProperty(newconfig,'sockets',{value:[],enumerable:true,writable:true});
+        else Object.defineProperty(newconfig,'sockets',{value:_config.sockets,enumerable:true,writable:true});
+
+        if(_config.emitname===undefined||typeof _config.emitname!=='string')Object.defineProperty(newconfig,'emitname',{value:flogname,enumerable:true,writable:false});
+        else Object.defineProperty(newconfig,'emitname',{value:_config.emitname,enumerable:true,writable:false});
+
+        if(_config.parents===undefined||typeof _config.parents!=='object')Object.defineProperty(newconfig,'parents',{value:[],enumerable:true,writable:true});
+        else Object.defineProperty(newconfig,'parents',{value:_config.parents,enumerable:true,writable:true});
+
+        if(_config.children===undefined||typeof _config.children!=='object')Object.defineProperty(newconfig,'children',{value:[],enumerable:true,writable:true});
+        else Object.defineProperty(newconfig,'children',{value:_config.children,enumerable:true,writable:true});
+
+        this.config=newconfig;
+        delete newconfig;
+        const self=this;
+
         if(this.config.readconsole){
-            let oldlog=console.log;console.log=function(){oldlog.apply(this,arguments);_print(_msg=Array.from(arguments),true);}
-            let olderr=console.error;console.error=function(){olderr.apply(console,arguments);_print(Array.from(arguments),true);}
-            let oldwarn=console.warn;console.warn=function(){oldwarn.apply(console,arguments);_print(Array.from(arguments),true);}
-            let olddeb=console.debug;console.debug=function(){olddeb.apply(console,arguments);_print(Array.from(arguments),true);}
+            let oldlog=console.log;
+            console.log=function(){
+                oldlog.apply(this,arguments);
+                _print(_msg=Array.from(arguments),true);
+            }
+            let olderr=console.error;
+            console.error=function(){
+                olderr.apply(console,arguments);
+                _print(Array.from(arguments),true);
+            }
+            let oldwarn=console.warn;
+            console.warn=function(){
+                oldwarn.apply(console,arguments);
+                _print(Array.from(arguments),true);
+            }
+            let olddeb=console.debug;
+            console.debug=function(){
+                olddeb.apply(console,arguments);
+                _print(Array.from(arguments),true);
+            }
         }
         if(this.config.sockets.length>0){
             let socks=this.config.sockets;
@@ -157,3 +198,4 @@ const flog=(()=>{
     };
 return{io};})();
 module.exports=flog;
+module.exports.default=flog;
--- README.md
+++ README.md
@@ -1,15 +1,26 @@
 # flog.io
 Logging Object For Sockets // Console And Nested Logging<br/>
-<br/>
+-
 flog.io is designed for nested print logging; helpful in areas such as admin of multiple chat rooms or game servers.<br/>
 The nested structure allows for the parent to log the output of its children over sockets.<br/>
 Replace your console.log() calls with yourvariable.print(), or use them together.<br/>
 
-npm install flog.io<br/>
+<br/>
+v3.0.1<br/>
+-
+---added remove() function
+
+<br/>
+
+npm install flog.io
+
+<br/>
 
 Create a new log by:<br/>
----const flog=require('flog.io');<br/>
----let yourvariable=new flog.io({config});<br/>
+---with or without a config object<br/>
+---const flog=require('flog.io')<br/>
+---let log=new flog.io({config object})<br/>
+<br/>
 
 Default Config Object:<br/>
 {<br/>
@@ -21,28 +32,39 @@ name:'flog.io'<br/>
 ,parents:[]<br/>
 ,children:[]<br/>
 }<br/>
+<br/>
 
-To enable parent-child structure logging of multiple logs, <br/>
----add a child log to a parent by -> yourvariable.addchild(child);<br/>
----add a parent log to a child by -> yourvariable.addparent(parent);<br/>
+Functions:<br/>
+print()<br/>
+addsocket(socket)<br/>
+addchild(child)<br/>
+addparent(parent)<br/>
+remove(socket||child||parent)<br/>
+<br/>
+Using print():<br/>
+---using a comma between arguments will start a new line<br/>
+---replace the use of console.log() with log.print(), print() will write to the console and send over sockets<br/>
+---or use console.log() and flog.io with config {readconsole:true} to also send over sockets<br/>
 
 To enable the socket logging:<br/>
----yourvariable.addsocket(socket);<br/>
----Once connected the log will continue to emit all yourvariable.print() messages through the sockets.<br/>
----The receiving message event by default is 'flog.io'<br/>
+---log.addsocket(socket) -> add a socket to pass print() arguments to using config.emitname<br/>
+---The receiving message event by default is config {emitname:'flog.io'}<br/>
 
-To enable logging of console.log:<br/>
----set 'readconsole=true'<br/>
----The log will bind to the console.log functions and print both the console output and yourvariable.print() calls.<br/>
+To enable parent-child structure logging of multiple logs,<br/>
+---log.addchild(child) -> add a child log, the child.print() calls will also call the parent.print()<br/>
+---log.addparent(parent) -> add a parent log, to receive child.print() calls<br/>
+
+To remove a socket, child, or parent:<br/>
+---log.remove() -> input a socket, child log, or parent log to remove from this log<br/>
+<br/>
 
 Future edits will include:<br/>
----(very soon) remove socket/child/parent<br/>
----asynchronous functions<br/>
----file logging<br/>
----temp line holding<br/>
+---error catching (in progress)<br/>
+---file logging (in progress)<br/>
+<br/>
 
 My apologies, versions 2.0.7 and below were hastily written.<br/>
-Version 3.0.0 is the 100% working version.<br/>
+Version 3.0.0 is the working version.<br/>
 All updates going forward should not be breaking.<br/>
 
 
